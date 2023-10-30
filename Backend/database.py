23fbfac5e9c200323d5fa80b4f9cfe2b8e1cc71d
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-10-30 22:32:01 +0300

    Create Table

--- Backend/database.py
+++ Backend/database.py
@@ -1,6 +1,40 @@
 import sqlite3
 
 
+import sqlite3
+
+
 def get_database_connection():
     connection = sqlite3.connect('mydatabase.db')
     return connection
+
+
+def create_table():
+    # Open a connection
+    connection = get_database_connection()
+    cursor = connection.cursor()
+
+    # Check if the table already exists
+    cursor.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='tasks';")
+    table_exists = cursor.fetchone()
+
+    if not table_exists:
+        # Create the table if it doesn't exist
+        cursor.execute('''
+            CREATE TABLE tasks (
+                id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                title TEXT ,
+                status TEXT 
+            )
+        ''')
+
+        # Commit and close the connection
+        connection.commit()
+
+    # Always close the connection
+    connection.close()
+
+
+# Call the create_table function to create the 'tasks' table (if it doesn't already exist)
+create_table()
