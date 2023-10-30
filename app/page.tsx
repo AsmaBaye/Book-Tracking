b6f85fd486a6e4fa5e5419a2af1c8dbc5d9e07db
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-10-30 22:02:55 +0300

    Refactor root app state lift and rendering comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+'use client'
+import { useState } from 'react';
+import BookPage from "./getbooks/page"
+import AddBook from './postbooks/page';
+
+export interface  Book {
+  id: number;
+  title: string;
+  status:string;
+}
 export default function Home() {
 
+  const [books, setBooks] = useState<Book[]>([]);
   return (
     <div>
        <h1>Add Book</h1>
+       <AddBook setBooks={setBooks} books={books}/>
+       <BookPage setBooks={setBooks} books={books}/>
      </div>
   )
 }
